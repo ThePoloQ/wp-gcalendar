fa3715d7dbece31add1e42eca8d79824b1bb6bb7
ThePoloQ <polosky.tdb@gmail.com>
2017-01-24 17:10:35 +0100

    Update gcalendar-param.php

--- gcalendar-param.php
+++ gcalendar-param.php
@@ -1,6 +1,6 @@
 <?php
 
-define('GCAL_APPLICATION_NAME', 'ceac-wavrin');
-define('GCAL_API_KEY', ' AIzaSyAKEo80L1bkrmcBICE-D38w8VbriZz6lPc');
-define('GCAL_CALENDAR_ID','boumb6955v31skv083guodt6l0@group.calendar.google.com');
+define('GCAL_APPLICATION_NAME', ''); // APP NAME
+define('GCAL_API_KEY', ''); //GENERATED API_KEY
+define('GCAL_CALENDAR_ID',''); //CALENDAR PUBLIC ID
 define('GCAL_MAX_RESULTS', 6);
